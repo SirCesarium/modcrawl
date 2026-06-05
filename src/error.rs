a0999862c5a01bc8946e775f61c3edb376fa9dc5
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-06-05 19:44:32 -0400

    feat: add Io and ZipCrawl error variants

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,18 @@
+use std::io;
+use std::result;
+
 use miette::Diagnostic;
 use thiserror::Error;
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Error, Diagnostic, Debug)]
 pub enum Error {
-    // ...
+    #[error("I/O error: {0}")]
+    #[diagnostic(code(modcrawl::io))]
+    Io(#[from] io::Error),
+
+    #[error("ZipCrawl error")]
+    #[diagnostic(code(modcrawl::zipcrawl))]
+    ZipCrawl(#[from] zipcrawl::ZipCrawlError),
 }
